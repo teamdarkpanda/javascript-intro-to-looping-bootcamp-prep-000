9ad538687186ea739c44d88f39f2ebf65951b1c5
Ankur Puri <apuri@iit.edu>
2018-05-15 22:01:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   for(var i=0; i<25; i++){
     if(i === 1) {
-      array.push(`I am${i} strange loop.`)
+      array.push(`I am ${i} strange loop.`)
     } else {
       array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
       }
